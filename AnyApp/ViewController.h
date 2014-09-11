f1ffec7feea7642c90922a4e0f9dce5e22d9dc6a
Bob Steers <kargathia@hotmail.com>
2014-09-11 10:31:28 +0200

    first push
    dfa

---
+++ AnyApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AnyApp
+//
+//  Created by Fhict on 11/09/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AnyApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AnyApp
+//
+//  Created by Fhict on 11/09/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
